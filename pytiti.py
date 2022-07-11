3e37b4f66226ed587cb9e52133f30858299c306f
RohanKP1 <95569542+RohanKP1@users.noreply.github.com>
2022-07-11 06:37:50 +0530

    added main file

---
+++ pytiti.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python3
+
+from os import system, name
+import sys
+import time
+
+class Gstructure():
+    
+    BOLD = 1
+    WHITE = 37
+    RED = 91
+    GREEN = 92
+    YELLOW = 93
+    BLUE = 94
+    GAME_MAP = [[ "" for _ in range(3)] for _ in range(3)]
+
+    def __init__(self) -> None:
+
+        if "-h" in self.check_argument():
+            self.clear()
+            print(f"""This is a simple {self.styled_text("TiC TAc TOE",Gstructure.BLUE,Gstructure.BOLD)} GamE
+
+> iN each turn you have to enter the coordinates of the cell to put your choice In,
+
+          coordinates are given as:
+
+     (0,0)    |    (0,1)    |    (0,2)    
+__________________________________________
+     (1,0)    |    (1,1)    |    (1,2)
+__________________________________________
+     (2,0)    |    (2,1)    |    (2,2)
+
+> How to put coordinates correctly:
+          Enter coordinates (x,y) : {self.styled_text("1,1",Gstructure.YELLOW,Gstructure.BOLD)}
+
+> Enter {self.styled_text("Ctrl+C",Gstructure.YELLOW,Gstructure.BOLD)} to exit the game anytime.            
+            """)
+
+
+        elif "-p" or "" in self.check_argument():     
+            choice = ["O","X"]
+            turn = 0
+            index = 0
+            while self.check_win(choice[index])[0] == 0:
+                self.clear()
+                print(self.styled_text("\t\t   Tik Tak Toe\n" ,Gstructure.YELLOW ,Gstructure.BOLD))
+                turn += 1
+                if turn%2 == 0:
+                    index = 1
+                    print(self.styled_text("\t\t    X\'s turn\n",Gstructure.BLUE))
+                else:
+                    index = 0
+                    print(self.styled_text("\t\t    O\'s turn\n",Gstructure.BLUE))    
+                self.print_game(Gstructure.GAME_MAP, len(Gstructure.GAME_MAP))
+                try:
+                    x,y = map(int, input("\n\nEnter coordinates (x,y) : ").split(","))
+                    if Gstructure.GAME_MAP[x][y] == "":
+                        self.update_chance(x,y,choice[index])
+                    else:
+                        print(self.styled_text("\nThe coordinate is already occupied!",Gstructure.RED,Gstructure.BOLD))
+                        time.sleep(2)
+                        turn -= 1
+                except IndexError:
+                        print(self.styled_text("\n\t\tIndex out of Range!",Gstructure.RED,Gstructure.BOLD))
+                        time.sleep(2)
+                        turn -= 1
+                except ValueError:
+                        print(self.styled_text("\nEnter the coordinates in specified format! i.e. (x,y)",Gstructure.RED,Gstructure.BOLD)) 
+                        time.sleep(2)
+                        turn -= 1         
+            self.clear()
+            print(self.styled_text("\t\t   Tik Tak Toe\n" ,Gstructure.YELLOW ,Gstructure.BOLD))      
+            self.print_game(Gstructure.GAME_MAP, len(Gstructure.GAME_MAP)) 
+
+            if self.check_win(choice[index])[0] == -1:
+                print(self.styled_text(f"\n\n\t\t   It\'s a DRAW!",Gstructure.GREEN,Gstructure.BOLD))
+            else:                
+                print(self.styled_text(f"\n\n\t\t     {choice[index]} won!",Gstructure.GREEN,Gstructure.BOLD))  
+
+    def styled_text(self, text: str, color: int = 0, style: int = 0) -> str:
+        return f"\033[{int(style)};{color}m{text}\033[39m"
+
+    def print_game(self, map: list, ran: int) -> str:
+        try:
+            for col in range (0,ran):
+                for row in range (0,ran):
+                    if row!=2:
+                        pole="\t|"
+                    else:
+                        pole=""    
+                    print("\t"+map[col][row]+pole,end="")
+                if col!=2: 
+                    print("\n"+"_"*50)
+        except IndexError:
+            print(self.styled_text("Index out of Range!",Gstructure.RED,Gstructure.BOLD))
+        
+
+    def update_chance(self, x: int, y: int, player: str = "X") -> None:
+        if Gstructure.GAME_MAP[x][y] == "":
+            Gstructure.GAME_MAP[x][y] = player
+        else:
+            pass                   
+
+    def check_win(self, choice : str) -> list:
+
+        super_list = Gstructure.GAME_MAP[0] + Gstructure.GAME_MAP[1] + Gstructure.GAME_MAP[2]
+        while "" in super_list:
+            for col in range (3):
+                if Gstructure.GAME_MAP[col][0] == Gstructure.GAME_MAP[col][1] ==  Gstructure.GAME_MAP[col][2] != "":
+                    Gstructure.GAME_MAP[col][0] = Gstructure.GAME_MAP[col][1] = Gstructure.GAME_MAP[col][2] = self.styled_text(choice,Gstructure.RED,Gstructure.BOLD)
+                    return [1,Gstructure.GAME_MAP[col][1]]
+
+            for row in range (3):
+                if Gstructure.GAME_MAP[0][row] == Gstructure.GAME_MAP[1][row] ==  Gstructure.GAME_MAP[2][row] != "":
+                    Gstructure.GAME_MAP[0][row] = Gstructure.GAME_MAP[1][row] = Gstructure.GAME_MAP[2][row] = self.styled_text(choice,Gstructure.RED,Gstructure.BOLD)
+                    return [1,Gstructure.GAME_MAP[1][row]]
+
+            if  Gstructure.GAME_MAP[0][0] == Gstructure.GAME_MAP[1][1] ==  Gstructure.GAME_MAP[2][2] != "":
+                Gstructure.GAME_MAP[0][0] = Gstructure.GAME_MAP[1][1] = Gstructure.GAME_MAP[2][2] = self.styled_text(choice,Gstructure.RED,Gstructure.BOLD)
+                return [1,Gstructure.GAME_MAP[1][1]]
+
+            elif Gstructure.GAME_MAP[0][2] == Gstructure.GAME_MAP[1][1] ==  Gstructure.GAME_MAP[2][0] != "":
+                Gstructure.GAME_MAP[0][2] = Gstructure.GAME_MAP[1][1] = Gstructure.GAME_MAP[2][0] = self.styled_text(choice,Gstructure.RED,Gstructure.BOLD)
+                return [1,Gstructure.GAME_MAP[1][1]]
+
+            else:
+                return [0] 
+        return [-1]                   
+
+    def clear(self) -> None:
+        if name == 'nt':
+                _ = system('cls')
+        else:
+                _ = system('clear') 
+
+    def check_argument(self) -> list:
+        return sys.argv[1:]  
+                             
+if __name__ == "__main__":
+    try:
+        Gstructure()
+    except KeyboardInterrupt:
+        exit(130)               
